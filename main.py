14aad91d9a655e0566fd48b80f58492e6e57aad2
Renatto Baca llontop <renatto.baca@telectronic.com.pe>
2021-06-23 09:22:52 -0500

    Actualizando cambios

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-from fastapi import Request, FastAPI, Response
-from BD import *
+from fastapi import Request, FastAPI
 
 app = FastAPI()
-
+"""
 #Url consultar paciente
 @app.post('/dniPaciente')
 async def consultaPaciente(request: Request):    
     body = await request.json()
     nombre = consultarPaciente(body['dni'])
-    return nombre
+    return nombre"""
 
 @app.get('/pruebita')
 async def pregunta():
